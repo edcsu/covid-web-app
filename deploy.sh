c01cf8268942742ae098f15d56e830c269e3f5e8
Keith Ssewanonda <keith@laboremus.no>
2020-04-06 00:57:27 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:edcsu/covid-web-app.git master:gh-pages
 
 cd -
