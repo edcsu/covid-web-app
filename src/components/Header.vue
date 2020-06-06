88b7d9562a2927afe15dea358757056b71a14d0f
Keith Ssewanonda <keith@laboremus.no>
2020-06-06 12:51:07 +0300

    style: added awesome bagdge recognition

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,24 @@
 
     <v-spacer></v-spacer>
 
-    <v-tooltip bottom>
+    <v-tooltip bottom nudge-left="150">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          href="https://github.com/NovelCOVID/awesome-novelcovid"
+          target="_blank"
+          icon
+          v-on="on"
+        >
+          <v-icon class="">mdi-seal</v-icon>
+        </v-btn>
+      </template>
+      <span>
+        Recognised on Awesome NovelCOVID projects
+        <v-icon class="">mdi-hand-pointing-up</v-icon>
+      </span>
+    </v-tooltip>
+
+    <v-tooltip bottom nudge-left="70">
       <template v-slot:activator="{ on }">
         <v-btn
           href="https://www.linkedin.com/in/ssewannonda-keith-edwin-443303129"
